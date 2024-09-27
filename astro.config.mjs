db2938a30f1a70c99316550dafccb842c1ec6123
juan4dev <juanindev@gmail.com>
2024-09-27 21:11:59 +0200

    feat: Add site configuration and deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://juan4dev.github.io',
+    base: 'sunflower-sun-dance',
+  })
